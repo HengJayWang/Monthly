96504d655b8d18069dabe960919d94c1a8543f6c
HengJay <hengjay.wang@itri.org.tw>
2023-01-12 14:58:17 +0800

    Initial Commit

---
+++ Monthly/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Monthly
+//
+//  Created by M100-M1MacMini on 2023/1/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by M100-M1MacMini on 2023/1/12.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
